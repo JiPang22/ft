40c4bb2566741a2f13e85c0171370a1bd62547b0
jp <wlvud6902@gmail.com>
2022-02-22 20:48:07 +0900

    update ft

--- model.f90
+++ model.f90
@@ -1,8 +1,9 @@
-program hook_FT
-integer*8 i, k, n
-real*8 dt, x, dx, v, dv, dw, w0, re, im
-parameter(dt = 1.e-2, dw = 1.e-2, w0 = 5.)
-real x_t(1000)
+program ft
+integer*8 i, k, n, imax, kmax, nmax
+real*8 dt, x, dx, v, dv, dw, w0, re, im, tmax, wmax
+parameter(dt = 1.e-2, dw = 1.e-1, w0 = 10., tmax = 1.e+2, wmax = 5.e+1)
+parameter(imax = int(tmax/dt), nmax = int(wmax/dw))
+real*8 x_t(imax)
 
 open(1,file='xt')
 open(2,file='vt')
@@ -11,22 +12,25 @@ open(4,file='xw')
 
 x = 0.; v = 1.
 
-do i = 1, 1000
+do i = 1, imax
 write(1,*) i * dt, x
 write(2,*) i * dt, v
 write(3,*) x, v
 x_t(i) = x; dx = v; dv = -w0**2 * x
 x = x + dx * dt; v = v + dv * dt
+print*, "step", 100 * i/imax, "%"
 enddo
 
-do n = 1, 1000! n is omega index 
-sum_re = 0.; sum_im = 0.
-do k = 1, 1000 ! k is time index in sumation loop 
-re = re + x_t(k) * cos(n * dw * k * dt) * dt
-im = im -x_t(k) * sin(n * dw * k * dt) * dt
+do n = 1, nmax! n is omega index 
+re = 0.; im = 0.
+
+do i = 1, imax 
+re = re + x_t(k) * cos(n * dw * i * dt) * dt
+im = im -x_t(k) * sin(n * dw * i * dt) * dt
 enddo
-write(4,*) n * dw, sqrt(sum_im**2 + sum_re**2)
-print*, "step... ", n
+
+write(4,*) n * dw, sqrt(re ** 2 + re ** 2)
+print*, "step", 100 * n/nmax, "%"
 enddo
 
 stop
