2c9877108de9fe276ac9a5f04a1338bcbe05254c
JiPang22 <wlvud6902@gmail.com>
2022-03-04 22:06:02 +0900

    iiii

--- d.f90
+++ d.f90
@@ -1,8 +1,8 @@
 program d 
 
 integer*8 i,n,imax,nmax
-real*8 x,v,dt,dx,dv,dw,gam,re,im,tmax,wmax,om0,om1,a0
-parameter(dt=1.e-2,dw=1.e-2,tmax=100.,wmax=10.,gam=0.5)
+real*8 x,v,dt,dx,dv,dw,tmax,wmax,om0,om1,a,gam
+parameter(dt=1.e-2,dw=1.e-2,tmax=50.,wmax=10.,gam=0.5,a=1.)
 parameter(imax=int(tmax/dt),nmax=int(wmax/dw),om0=0.5,om1=0.5)
 open(1,file='xt')
 x=1.;v=0.
