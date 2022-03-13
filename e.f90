49b40e7d676590de1da8469421b0992edf31d655
JiPang22 <wlvud6902@gmail.com>
2022-03-13 22:12:24 +0900

    push update e9

--- e.f90
+++ e.f90
@@ -2,12 +2,17 @@ program e
 
 integer*8 i,j,imax,jmax
 real*8 x,v,dt,dx,dv,dw,tmax,wmax,om0,om1,a,gam
-parameter(dt=1.e-2,dw=1.e-2,tmax=50.,wmax=10.,gam=1.5,a=1.)
-parameter(imax=int(tmax/dt),jmax=int(wmax/dw),om0=1.,om1=1.)
+parameter(dt=1.e-2,dw=1.e-2,tmax=20.,wmax=20.)
+parameter(imax=int(tmax/dt),jmax=int(wmax/dw))
 real xt(imax)
 open(1,file='xt')
 open(2,file='xw')
-x=0.;v=0.5
+
+!system parameter
+parameter(gam=1.,a=1,om0=10.,om1=2.)
+
+!initial condition
+x=0.1;v=0.
 
 do i=1,imax
 dx=v; dv=-(om0**2)*x-2*gam*v+a*cos(om1*i*dt)
